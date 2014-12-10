66e3e0581ce532fe785911e9e5a2571550b53d31
Eric Whyne <ericwhyne@gmail.com>
2014-12-09 21:52:00 -0500

    benchmarked

--- python-strcat-bm/strcat.py
+++ python-strcat-bm/strcat.py
@@ -1,25 +1,50 @@
 #!/usr/bin/python
 import sys
 import cProfile
+import time
+import memory_profiler
 
+i = 10000000
+tladd = open("timelog-add.csv",'w')
+tllst = open("timelog-lst.csv",'w')
+j = 0
 
+def timefunc(f):
+    def f_timer(*args, **kwargs):
+        start = time.time()
+        result = f(*args, **kwargs)
+        end = time.time()
+        print f.__name__, 'took', end - start, 'time'
+        if f.__name__ == 'stradd':
+          tladd.write(str(j) + "," + str(end-start) + "\n")
+        elif f.__name__ == 'strlist':
+          tllst.write(str(j) + "," + str(end-start) + "\n")
+        return result
+    return f_timer
 
-i = 100000
-
+@timefunc
+@profile
 def stradd(i):
   string = ""
-  for _ in range(i):
+  for _ in xrange(i):
     string += "+"
+  print "string length ", len(string)
 
+@timefunc
 def strlist(i):
   string = ""
   appendlist = []
-  for _ in range(i):
-    appendlist.append("+")
-  string.join(appendlist)
+  for _ in xrange(i):
+    appendlist.append("-")
+  string = string.join(appendlist)
+  print "string length ", len(string)
 
-print "\n\nadding strings"
-addprofile = cProfile.run('stradd(i)')
+print "using a list"
+for k in range(0,i,i//100):
+  j = k
+  strlist(j)
 
-print "\n\nusing a list"
-listprofile = cProfile.run('strlist(i)')
+print "adding strings"
+for k in range(0,i,i//100):
+  j = k
+  stradd(j)
